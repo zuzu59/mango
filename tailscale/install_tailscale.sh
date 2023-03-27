13e30cbf5f18a5485a553f12c0d02b2e6bcb8a05
Christian Zufferey <christian@zufferey.com>
2023-03-27 16:05:53 +0000

    new: ajouté un script pour installer Tailscale, ne fonctionne pas encore !

--- tailscale/install_tailscale.sh
+++ tailscale/install_tailscale.sh
@@ -1,15 +1,42 @@
 #!/bin/ash
 #Petit script pour installer Tailscale facilement en semi auto
 #
-#zf230322.1928, zf230327.1721
+#zf230322.1928, zf230327.1800
 
 echo ""
 read -p "Attention il faut se trouver dans le dossier 'tailscale' !"
+read -p "Vous êtes sûr ?"
+echo ""
 
-cp -r ./etc/ /
+opkg install kmod-tun iptables-nft
 
-rm /usr/sbin/tailscale
-#ln -s /mnt/sda1/tailscale/tailscale_1.38.1_mipsle/tailscale /usr/sbin/tailscale
+rm /usr/sbin/tailscalecp -r ./etc/ /
+
+rm -f /usr/sbin/tailscale
+
+mkdir bin
+cd bin
+
+wget https://pkgs.tailscale.com/stable/tailscale_1.38.2_mipsle.tgz
+tar -zxvf tailscale_1.38.2_mipsle.tgz
+
+rm -f /usr/sbin/tailscale
+ln -s /mnt/sda1/mango/tailscale/bin/tailscale_1.38.2_mipsle/tailscale /usr/sbin/tailscale
+
+rm -f /usr/sbin/tailscaled
+ln -s /mnt/sda1/mango/tailscale/bin/tailscale_1.38.2_mipsle/tailscaled /usr/sbin/tailscaled
+
+/etc/init.d/tailscale stop
+/etc/init.d/tailscale start
+
+tailscale up --advertise-routes=192.168.0.0/24 --accept-routes --advertise-exit-node�---netfilter-mode=off
+
+echo -e "
+
+Voila, c'est tout fait !
+"
+
+
 
 
 exit
@@ -17,18 +44,6 @@ exit
 
 
 
-
-
-opkg update
-opkg install tree htop nano sudo
-
-cp -r ./openwrt/etc/ /
-
-rm /usr/sbin/tailscale
-ln -s /mnt/sda1/tailscale/tailscale_1.38.1_mipsle/tailscale /usr/sbin/tailscale
-
-rm /usr/sbin/tailscaled
-ln -s /mnt/sda1/tailscale/tailscale_1.38.1_mipsle/tailscaled /usr/sbin/tailscaled
 
 echo 'net.ipv4.ip_forward = 1' | sudo tee /etc/sysctl.d/99-tailscale.conf
 echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
