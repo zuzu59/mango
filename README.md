be0c54c5f77f0d1b0c9d61e01504f823db6e402b
Christian Zufferey <christian@zufferey.com>
2023-03-27 13:58:35 +0000

    refact: refact le readme et commencé install.sh

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # mango
 Mes petits utilitaires sympa pour le mini routeur MANGO
 
-zf230327.1549
+zf230327.1557
 
 # Install
 Il faut en premier installer GIT dans le routeur MANGO pour après pouvoir faire le git clone de manière temporaire dans la RAM
@@ -10,7 +10,12 @@ opkg update && opkg remove git &&
 
 opkg -d RAM install git
 
-puis cloner avec un github ssh !
+puis cloner avec un github ssh mais en RAM pour commencer !
+
+cd /tmp
+git clone git@github.com:zuzu59/mango.git
+cd mango
+install_utils.sh
 
 
 
---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/ash
+#Petit script pour installer un mini routeur MANGO facilement
+#
+#zf230327.1555
+
+# installation des outils de base minimum
+opkg update
+opkg install tree htop nano-full sudo
+
+# installation du montage de la clef USB automatiquement lros du boot
+opkg install block-mount e2fsprogs kmod-fs-ext4 kmod-usb-storage kmod-usb2 kmod-usb3
+block detect | uci import fstab
+uci set fstab.@mount[0].enabled='1' && uci set fstab.@global[0].anon_mount='1' && uci commit fstab
+/etc/init.d/fstab boot
+
+
+
+
+exit
+
+
+
+
+
+
+echo ""
+read -p "Attention il faut se trouver dans le dossier 'tailscale' !"
+
+opkg update
+opkg install tree htop nano sudo
+
+cp -r ./openwrt/etc/ /
+
+rm /usr/sbin/tailscale
+ln -s /mnt/sda1/tailscale/tailscale_1.38.1_mipsle/tailscale /usr/sbin/tailscale
+
+rm /usr/sbin/tailscaled
+ln -s /mnt/sda1/tailscale/tailscale_1.38.1_mipsle/tailscaled /usr/sbin/tailscaled
+
+echo 'net.ipv4.ip_forward = 1' | sudo tee /etc/sysctl.d/99-tailscale.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
+
+/etc/init.d/tailscale stop
+/etc/init.d/tailscale start
+
+tailscale up --advertise-routes=192.168.0.0/24 --accept-routes --advertise-exit-nodeâ---netfilter-mode=off
+
+echo -e "
+
+Voila, c'est tout fait !
+"
+
