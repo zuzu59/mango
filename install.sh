fa982411fdbb1ebd913c725fcf94b6279dc8806a
Christian Zufferey <christian@zufferey.com>
2023-04-05 10:54:05 +0200

    fix: ajouté le driver UAS pour le disque SSD M.2 USB3, car il manquait

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/ash
 # Petit script pour installer un mini routeur MANGO facilement
 #
-# zf230327.1616, zf230404.1354
+# zf230327.1616, zf230405.1052
 
 # installation des outils de base minimum
 opkg update
 opkg install tree htop nano-full
 
 # installation et configuration du montage de la clef USB automatiquement lors du boot
-opkg install block-mount e2fsprogs kmod-fs-ext4 kmod-usb-storage kmod-usb2 kmod-usb3 usbutils gdisk
+opkg install block-mount e2fsprogs kmod-fs-ext4 kmod-usb-storage kmod-usb2 kmod-usb3 usbutils gdisk kmod-usb-storage-uas
 
 block detect | uci import fstab
 uci set fstab.@mount[0].enabled='1' && uci set fstab.@global[0].anon_mount='1' && uci commit fstab
