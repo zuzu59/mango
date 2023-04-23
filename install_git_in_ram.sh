45c188b5738bbdaf949b4592f5b01605c581a37e
Christian Zufferey <christian@zufferey.com>
2023-04-23 20:51:29 +0200

    change: Amélioré l'installation du GIT en RAM et supprimé l'installation minimaliste dans un Mango OpenWRT car l'écriture sur la clef USB est beaucoup trop lente

--- install_git_in_ram.sh
+++ install_git_in_ram.sh
@@ -1,7 +1,12 @@
 #!/bin/ash
 # Petit script pour installer facilement git dans la RAM afin de ne pas utiliser la FLASH
 # Utilie surtout après un reboot quand on veut pusher un commit !
-#zf230327.1803, zf230404.1410
+#zf230327.1803, zf230423.2050
+
+echo ""
+read -p "Attention il faut faire source ./install_git_in_ram.sh !"
+read -p "Etes-vous certains ?"
+echo ""
 
 opkg update
 opkg install zlib
