c6c020db39512f0e9b3ab1d1695e4f7d4aa2d135
Christian Zufferey <christian@zufferey.com>
2024-07-01 14:29:58 +0200

    change: supprimé la partie openssh-sftp-avahi-service luci-proto-relay luci-app-attendedsysupgrade dbus

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/ash
 # Petit script pour installer un mini routeur MANGO facilement
 #
-# zf230327.1616, zf230411.1423, zf240403.1338
+# zf230327.1616, zf230411.1423, zf240701.1428
 
 # installation des outils de base minimum
 opkg update
-opkg install tree htop nano-full bash
+opkg install tree htop nano-full bash diffutils
 
 # installation et configuration du montage de la clef USB automatiquement lors du boot
 opkg install block-mount e2fsprogs kmod-fs-ext4 kmod-usb-storage kmod-usb2 kmod-usb3 usbutils gdisk kmod-usb-storage-uas
@@ -15,14 +15,14 @@ uci set fstab.@mount[0].enabled='1' && uci set fstab.@global[0].anon_mount='1' &
 /etc/init.d/fstab boot
 
 # installation des outils intéressants (SFTP, Relayd, Upgrade Attended Sysupgrade)
-opkg install openssh-sftp-avahi-service luci-proto-relay luci-app-attendedsysupgrade dbus
+# opkg install openssh-sftp-avahi-service luci-proto-relay luci-app-attendedsysupgrade dbus
 
 # configuration du réflecteur mDNS
-sed -i -e "s/enable-reflector\=no/enable-reflector\=yes/g" /etc/avahi/avahi-daemon.conf
-/etc/init.d/dbus start
-/etc/init.d/dbus enable
-/etc/init.d/avahi-daemon start
-/etc/init.d/avahi-daemon enable
+#sed -i -e "s/enable-reflector\=no/enable-reflector\=yes/g" /etc/avahi/avahi-daemon.conf
+#/etc/init.d/dbus start
+#/etc/init.d/dbus enable
+#/etc/init.d/avahi-daemon start
+#/etc/init.d/avahi-daemon enable
 
 
 
